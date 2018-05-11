1a189ba185b17a8a29d8c839c4e43feac9e97851
alexeyraga <alexey.raga@gmail.com>
2018-05-11 11:05:06 +1000

    Store and commit offsets

--- src/Arbor/TopicTap/TConsumer.hs
+++ src/Arbor/TopicTap/TConsumer.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Arbor.TopicTap.TConsumer
 ( TConsumer
 , createConsumer
@@ -19,13 +20,14 @@ import Arbor.TopicTap.Error
 import Arbor.TopicTap.Kafka
 import Arbor.TopicTap.Options
 
-data TConsumer m = TConsumer KafkaConsumer (IORef (TC m))
+data TConsumer m = TConsumer
+  { tcKafka :: KafkaConsumer
+  , tcFuncs :: IORef (TC m)
+  }
 
 data TC m = TC
-  { pollMessage      :: m (Either TopicTapError (ConsumerRecord (Maybe ByteString) (Maybe ByteString)))
-  , close            :: m (Maybe TopicTapError)
-  , commitOffsets    :: [TopicPartition] -> m (Maybe TopicTapError)
-  , commitAllOffsets :: m (Maybe TopicTapError)
+  { pollMessage :: m (Either TopicTapError (ConsumerRecord (Maybe ByteString) (Maybe ByteString)))
+  , close       :: m (Maybe TopicTapError)
   }
 
 -- | Creates a new TopicTap consumer.
@@ -38,16 +40,41 @@ createConsumer :: MonadIO m
                -> m (Either TopicTapError (TConsumer m))
 createConsumer conf topic cgroup onRebalance = do
   kafka <- createKafkaConsumer conf topic cgroup onRebalance
-  mapM (mkTConsumer (_kcPollTimeoutMs conf) ) kafka
+  mapM (mkTConsumer (_kcPollTimeoutMs conf)) kafka
 
 -- | Closes the TopicTap consumer.
 closeConsumer :: MonadIO m => TConsumer m -> m (Maybe TopicTapError)
-closeConsumer (TConsumer kafka iotc) = do
-  tc <- liftIO $ readIORef iotc
+closeConsumer TConsumer{tcKafka, tcFuncs} = do
+  tc    <- liftIO $ readIORef tcFuncs
   tcErr <- close tc
   case tcErr of
     Just err -> pure (Just err)
-    Nothing  -> closeKafkaConsumer kafka
+    Nothing  -> closeKafkaConsumer tcKafka
+
+-- | Stores offsets to be committed later by 'commitOffsets'
+storeOffsets :: MonadIO m
+             => TConsumer m
+             -> [TopicPartition]
+             -> m (Maybe TopicTapError)
+storeOffsets TConsumer{tcKafka} =
+  fmap (fmap KafkaErr) . K.storeOffsets tcKafka
+
+-- | Stores specific message offset to be committed later
+-- when 'commitOffsets' is called
+storeMessageOffset :: MonadIO m
+                   => TConsumer m
+                   -> ConsumerRecord k v
+                   -> m (Maybe TopicTapError)
+storeMessageOffset TConsumer{tcKafka} cr =
+  fmap KafkaErr <$> K.storeOffsetMessage tcKafka cr
+
+--  | Commits all the stored offsets
+commitOffsets :: MonadIO m
+              => TConsumer m
+              -> m (Maybe TopicTapError)
+commitOffsets TConsumer{tcKafka} =
+  fmap KafkaErr <$> K.commitAllOffsets K.OffsetCommit tcKafka
+
 -------------------------------------------------------------------------------
 mkTConsumer :: MonadIO m => Timeout -> KafkaConsumer -> m (TConsumer m)
 mkTConsumer timeout consumer =
@@ -57,6 +84,4 @@ kafkaTC :: MonadIO m => Timeout -> KafkaConsumer -> TC m
 kafkaTC timeout consumer = TC
   { pollMessage       = left KafkaErr <$> K.pollMessage consumer timeout
   , close             = pure Nothing
-  , commitOffsets     = fmap (fmap KafkaErr) . K.commitPartitionsOffsets K.OffsetCommit consumer
-  , commitAllOffsets  = fmap KafkaErr <$> K.commitAllOffsets K.OffsetCommit consumer
   }
